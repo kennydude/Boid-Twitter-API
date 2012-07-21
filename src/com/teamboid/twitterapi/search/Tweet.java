f81a32d5349ae862760cb23d6b1af2a5c1590a55
Aidan Follestad <drummer.aidan@gmail.com>
2012-07-21 14:17:58 -0500

    Removed unnecessary folders and updated the .gitignore file.

--- src/com/teamboid/twitterapi/search/Tweet.java
+++ src/com/teamboid/twitterapi/search/Tweet.java
@@ -1,9 +1,31 @@
 package com.teamboid.twitterapi.search;
 
+import com.teamboid.twitterapi.status.GeoLocation;
+
+import java.util.Calendar;
+
 /**
  * Represents a Tweet held in search results.
  */
 public interface Tweet {
 
+    Calendar getCreatedAt();
 
+    String getProfileImageUrl();
+
+    long getFromUserId();
+
+    String getFromUser();
+
+    String getText();
+
+    long getToUserId();
+
+    long getId();
+
+    GeoLocation getGeo();
+
+    String getIsoLanguageCode();
+
+    String getSource();
 }
